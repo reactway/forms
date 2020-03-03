ee6d280ba9389dc9c949f5d79222c2ac5500ba4a
Martynas Zilinskas <martynas@quatrodev.com>
2020-03-03 16:56:04 +0200

    Added peerDependencies in rollup config.

--- packages/forms/rollup.config.js
+++ packages/forms/rollup.config.js
@@ -3,4 +3,6 @@ import { rollupConfigFactory } from "@reactway-tools/rollup";
 
 const packageJson = require("./package.json");
 
-export default rollupConfigFactory({ dependencies: Object.keys(packageJson.dependencies) });
+export default rollupConfigFactory({
+    dependencies: [...Object.keys(packageJson.dependencies || {}), ...Object.keys(packageJson.peerDependencies || {})]
+});
