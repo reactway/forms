baf459a4137799ad77e6ab90a1de922d821af764
Martynas Zilinskas <martynas@quatrodev.com>
2020-03-03 16:40:31 +0200

    WIP. forms-core build:prod with rollup.

---
+++ tools/rollup/src/rollup-config-factory.ts
@@ -0,0 +1,28 @@
+import { InputOptions, OutputOptions } from "rollup";
+import commonjs from "@rollup/plugin-commonjs";
+import typescript from "@rollup/plugin-typescript";
+import builtins from "builtin-modules";
+import { terser } from "rollup-plugin-terser";
+
+export interface RollupConfigFactoryOptions {
+    dependencies: string[];
+}
+
+export function rollupConfigFactory(options: RollupConfigFactoryOptions): InputOptions & { output: OutputOptions } {
+    return {
+        input: "src/index.ts",
+        output: {
+            file: "dist/bundle.js",
+            format: "cjs"
+        },
+        external: [...builtins, ...options.dependencies],
+        // prettier-ignore
+        plugins: [
+            commonjs(),
+            typescript({
+                composite: false
+            }),
+            terser()
+        ]
+    };
+}
